b9d5631c88a5022a7058fae923584b72d2a6a35f
CodexisPhantom <gabdostuni47400@gmail.com>
2024-12-25 10:42:38 +0100

    fix: getting correct names from api

--- FivemMapsFixer/Models/Settings.cs
+++ FivemMapsFixer/Models/Settings.cs
@@ -3,5 +3,5 @@ namespace FivemMapsFixer.Models;
 public static class Settings
 {
     public static string Path { get; set; } = "/media/Pgm/Download";
-    public static string ApiNameUrl => "https://joaat.sh/api/convert";
+    public static string ApiNameUrl => "https://joaat.sh/j/convert";
 }
